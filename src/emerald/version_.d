8e7afa7c06ce458979efba7dd9182a068ed9ccc0
pvmoore <1926576+pvmoore@users.noreply.github.com>
2021-01-24 14:06:09 +0000

    Tidu up

--- src/emerald/version_.d
+++ src/emerald/version_.d
@@ -1,12 +1,13 @@
 module emerald.version_;
 
-enum VERSION = "0.12";
+enum VERSION = "0.13";
 
 /**
  * ##################################################################
  * HISTORY
  * ##################################################################
  *
+ * 0.13 - TBC
  * 0.12 - Enable Spheres and materials for GPU path tracer
  * 0.11 - Optimise software path tracer slightly
  *      - Start to implement GPU path tracer
@@ -24,7 +25,16 @@ enum VERSION = "0.12";
  * ##################################################################
  * TODO
  * ##################################################################
- * - Create a bubble sphere with eta=1 (same as air)
  * - Add a skybox (and add an outdoor scene to demonstrate it)
  * - Run averaging pass every few iterations
+ * - Add multisample to GPU path tracer
+ *
+ * - Acceleration structure for GPU
+ *   ------------------------------
+ *   Divide the world space into blocks. Each block contains a pointer to a list of shapes
+ *   that have some part of their geometry inside that block. We will need to use ray marching
+ *   to march through the blocks and test each shape in that block. We can also mark each shape
+ *   that has already been tested so that we don't do any shape twice.
+ *   We may end up testing more shapes this way but it might be faster due to not having to
+ *   navigate a BVH tree.
  */
